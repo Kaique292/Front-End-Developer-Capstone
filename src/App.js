26f7e6f65140a15781b8cdaab8739f79969b944c
Wolf Code <kaique_amaral@usp.br>
2023-07-18 16:31:30 -0300

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import './App.css'
+import { useState, useEffect, React } from 'react'
+import { Navbar, Header, Main, Footer, Reservations, OrderOnline } from './components'
+import { Routes, Route, useLocation } from "react-router-dom"
+import { headerData, reservationData, orderOnline } from './constants'
+import DataContext from './DataContext'
+import ConfirmedBooking from './components/Reservations/ConfirmedBooking'
 
 function App() {
+    const location = useLocation();
+    let data = {}
+
+    useEffect(() => {
+        switch (location.pathname) {
+            case '/':
+                data = headerData
+                return
+            case '/Little-Lemon':
+                document.title = 'Little Lemon'
+                return
+            case '/Reservations':
+                data = reservationData
+                return
+            case '/OrderOnline':
+                data = orderOnline
+                return
+        }
+    }, [location.pathname])
+
     return (
-        <div className="App">
-            <header>
-                <img src='' alt='logo' />
-                <nav>
-                    <ul>
-                        <li><a href='#'></a></li>
-                        <li><a href='#'></a></li>
-                        <li><a href='#'></a></li>
-                        <li><a href='#'></a></li>
-                    </ul>
-                </nav>
-            </header>
-            <main></main>
-            <footer></footer>
-        </div>
-    )
+        <>
+            <Navbar />
+            <DataContext.Provider value={data}>
+                <Routes>
+                    <Route path='/Little-Lemon' element={
+                        <>
+                            <Header />
+                            <Main />
+                        </>
+                    } />
+                    <Route path='/' element={
+                        <>
+                            <Header />
+                            <Main />
+                        </>
+                    } />
+                    <Route path='/Reservations' element={
+                        <>
+                            <Header />
+                            <Reservations />
+                        </>
+                    } />
+                    <Route path='/OrderOnline' element={
+                        <>
+                            <Header />
+                            <OrderOnline />
+                        </>
+                    } />
+
+                    <Route path='/ConfirmedBooking' element={<ConfirmedBooking />} />
+                </Routes>
+            </DataContext.Provider>
+            <Footer />
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/Reservations/BookingForm.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import GuestSelector from './GuestSelector/GuestSelector';
+import DateSelecotr from './DateSelector/DateSelector';
+import TimeSelector from './TimeSelector/TimeSelector';
+
+import Reserve from './Reserve/Reserve';
+
+function BookingForm(props) {
+  const [guests, setGuests] = useState(1);
+  const [ocassion, setOccasion] = useState('birthday');
+
+
+  const [reservation, setReservation] = useState({ guests: guests, date: '', time: '', ocassion: ocassion });
+
+  const chooseTime = (time) => {
+    setReservation({ ...reservation, time: time });
+  }
+
+  const chooseGuest = (guests) => {
+    setGuests(guests);
+    setReservation({ ...reservation, guests: guests });
+  }
+
+  const chooseDate = (date) => {
+    setReservation({ ...reservation, date: date });
+
+    props.dispatchTimeslotsOnDateChange(date);
+
+  }
+
+  const chooseOcassion = (ocassion) => {
+    setOccasion(ocassion);
+
+    setReservation({ ...reservation, ocassion: ocassion });
+  }
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    props.submitReservation(reservation);
+  }
+
+
+  const validateReservation = () => {
+    if (reservation.time !== '' &&
+      reservation.date !== '' &&
+      reservation.guests !== '' &&
+      reservation.ocassion !== '') {
+      return true;
+    }
+
+    return false;
+  }
+
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <GuestSelector chooseGuest={chooseGuest} />
+
+        <DateSelecotr chooseDate={chooseDate} chooseOcassion={chooseOcassion} ocassion={ocassion} />
+
+        <TimeSelector chooseTime={chooseTime} availableTimeSlots={props.availableTimeSlots} />
+
+        {
+          validateReservation() ? <Reserve value={0} /> : <Reserve value={1} />
+        }
+      </form>
+    </div>
+  );
+};
+
+export default BookingForm;
