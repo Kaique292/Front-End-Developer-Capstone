792f4f494f7bfa3a43af8f361bb5480db0045ddb
Wolf Code <kaique_amaral@usp.br>
2023-07-18 14:59:55 -0300

    featured: meta tags and semantic tags

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>
-        Homepage
-      </h1>
-    </div>
-  )
+    return (
+        <div className="App">
+            <header>
+                <img src='' alt='logo' />
+                <nav>
+                    <ul>
+                        <li><a href='#'></a></li>
+                        <li><a href='#'></a></li>
+                        <li><a href='#'></a></li>
+                        <li><a href='#'></a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main></main>
+            <footer></footer>
+        </div>
+    )
 }
 
 export default App
